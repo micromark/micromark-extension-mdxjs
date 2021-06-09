412db023e1dc1fc7d6960d1363b6273f584ff8b5
Titus Wormer <tituswormer@gmail.com>
2021-06-09 16:22:30 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
+/**
+ * @typedef {import('micromark-util-types').Extension} Extension
+ *
+ * to do: {import('micromark-util-mdx-jsx').Options} Options
+ * @typedef {Record<string, unknown>} Options
+ */
+
 import {Parser} from 'acorn'
+// @ts-expect-error: untyped
 import acornJsx from 'acorn-jsx'
 import {combineExtensions} from 'micromark-util-combine-extensions'
+// @ts-expect-error: next
 import expression from 'micromark-extension-mdx-expression'
+// @ts-expect-error: next
 import jsx from 'micromark-extension-mdx-jsx'
+// @ts-expect-error: next
 import md from 'micromark-extension-mdx-md'
+// @ts-expect-error: next
 import esm from 'micromark-extension-mdxjs-esm'
 
+/**
+ * @param {Options} [options]
+ * @returns {Extension}
+ */
 export function mdxjs(options) {
   const settings = Object.assign(
     {
@@ -16,6 +32,7 @@ export function mdxjs(options) {
     options
   )
 
+  // @ts-expect-error: It’s fine.
   return combineExtensions([
     esm(settings),
     expression(settings),
