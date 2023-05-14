4158a3c0c288f58a40b6bbeb17bd141a32c6fe2e
Titus Wormer <tituswormer@gmail.com>
2023-05-14 15:35:07 +0200

    Use Node test runner

--- test.js
+++ test.js
@@ -1,19 +1,18 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {micromark} from 'micromark'
 import {mdxjs} from './index.js'
 
-test('markdown -> html (micromark)', (t) => {
-  t.deepEqual(
+test('markdown -> html (micromark)', function () {
+  assert.equal(
     micromark('<div>\n{asd}\n</div>', {extensions: [mdxjs()]}),
     '\n\n',
     'should work'
   )
 
-  t.deepEqual(
+  assert.equal(
     micromark('<x {...{"{": 1}} />\n{1 + /*}*/ + 2}', {extensions: [mdxjs()]}),
     '\n',
     'should be gnostic'
   )
-
-  t.end()
 })
