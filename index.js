ae572b468ec51c0ec3b248376d9750d6f10a7b5d
Titus Wormer <tituswormer@gmail.com>
2023-05-14 15:32:52 +0200

    Refactor code-style

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 /**
- * @typedef {import('micromark-util-types').Extension} Extension
  * @typedef {import('micromark-extension-mdx-expression').Options} Options
+ * @typedef {import('micromark-util-types').Extension} Extension
  */
 
 import {Parser} from 'acorn'
 import acornJsx from 'acorn-jsx'
-import {combineExtensions} from 'micromark-util-combine-extensions'
 import {mdxExpression} from 'micromark-extension-mdx-expression'
 import {mdxJsx} from 'micromark-extension-mdx-jsx'
 import {mdxMd} from 'micromark-extension-mdx-md'
 import {mdxjsEsm} from 'micromark-extension-mdxjs-esm'
+import {combineExtensions} from 'micromark-util-combine-extensions'
 
 /**
- * @param {Options} [options]
+ * Create an extension for `micromark` to enable MDX syntax.
+ *
+ * @param {Options | null | undefined} [options]
+ *   Configuration (optional).
  * @returns {Extension}
+ *   Extension for `micromark` that can be passed in `extensions` to enable MDX
+ *   syntax.
  */
 export function mdxjs(options) {
   const settings = Object.assign(
