72eb7067449ee67e47207331a471328f0dbf28a1
Titus Wormer <tituswormer@gmail.com>
2023-06-22 17:18:44 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -3,23 +3,28 @@ import test from 'node:test'
 import {micromark} from 'micromark'
 import {mdxjs} from './index.js'
 
-test('markdown -> html (micromark)', async function () {
-  assert.deepEqual(
-    // To do: next major: use export map, use package name here.
-    Object.keys(await import('./index.js')).sort(),
-    ['mdxjs'],
-    'should expose the public api'
-  )
+test('markdown -> html (micromark)', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(
+      // To do: next major: use export map, use package name here.
+      Object.keys(await import('./index.js')).sort(),
+      ['mdxjs']
+    )
+  })
 
-  assert.equal(
-    micromark('<div>\n{asd}\n</div>', {extensions: [mdxjs()]}),
-    '\n\n',
-    'should work'
-  )
+  await t.test('should work', async function () {
+    assert.equal(
+      micromark('<div>\n{asd}\n</div>', {extensions: [mdxjs()]}),
+      '\n\n'
+    )
+  })
 
-  assert.equal(
-    micromark('<x {...{"{": 1}} />\n{1 + /*}*/ + 2}', {extensions: [mdxjs()]}),
-    '\n',
-    'should be gnostic'
-  )
+  await t.test('should be gnostic', async function () {
+    assert.equal(
+      micromark('<x {...{"{": 1}} />\n{1 + /*}*/ + 2}', {
+        extensions: [mdxjs()]
+      }),
+      '\n'
+    )
+  })
 })
