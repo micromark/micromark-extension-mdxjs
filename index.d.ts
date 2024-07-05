28b1f24bae9f71573b290e2de6e26ed22da19a1f
Titus Wormer <tituswormer@gmail.com>
2024-07-05 17:46:00 +0200

    Refactor to use `@import`s

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import type {Options as MdxExpressionOptions} from 'micromark-extension-mdx-expression'
+
+export {mdxjs} from './lib/index.js'
+
+/**
+ * Configuration for `mdxjs` of `micromark-extension-mdxjs`.
+ */
+export interface Options extends MdxExpressionOptions {}
